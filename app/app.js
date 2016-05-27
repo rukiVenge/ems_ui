f0c390920a0db009e891f7d2f1b18d123e1755d2
Mark Kelvin Pineda <mpineda@exist.com>
2016-05-27 13:46:25 +0800

    -Create a standard file directory for each module. -Separate controller.js from app.js

--- app/app.js
+++ app/app.js
@@ -1,37 +1,34 @@
-'use strict';
 
-// Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ui.router',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
-	$urlRouterProvider.otherwise("/state1");
-	
-	$stateProvider
-    .state('state1', {
-      url: "/state1",
-      templateUrl: "login1.html"
-    })
-    .state('state1.list', {
-      url: "/list",
-      templateUrl: "login1_list.html",
-      controller: function($scope) {
-        $scope.items = ["A", "List", "Of", "Items"];
-      }
-    })
-    .state('state2', {
-      url: "/state2",
-      templateUrl: "login2.html"
-    })
-    .state('state2.list', {
-        url: "/list",
-        templateUrl: "login2_list.html",
-        controller: function($scope) {
-          $scope.things = ["A", "Set", "Of", "Things"];
-        }
-      });
-	
-}]);
+	// Declare app level module which depends on views, and components
+	angular.module('myApp', [
+	  'ui.router',
+	]).
+	config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+		$urlRouterProvider.otherwise('/state1');
+		
+		$stateProvider
+	    .state('state1', {
+	      url: '/state1',
+	      templateUrl: 'module/login/view/login1.html',
+	      controller: 'LoginController'
+	    })
+	    .state('state1.list', {
+	      url: '/list',
+	      templateUrl: '/module/login/view/login1_list.html',
+	      controller: function($scope) {
+	        $scope.items = ["A", "List", "Of", "Items"];
+	      }
+	    })
+	    .state('state2', {
+	      url: '/state2',
+	      templateUrl: '/module/login/view/login2.html'
+	    })
+	    .state('state2.list', {
+	        url: '/list',
+	        templateUrl: '/module/login/view/login2_list.html',
+	        controller: function($scope) {
+	          $scope.things = ["A", "Set", "Of", "Things"];
+	        }
+	      });
+		
+	}]);
